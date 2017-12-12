6790287c0bedb61c269bddbf69a21b53f4a9e958
danielsnider <danielsnider12@gmail.com>
2017-12-12 14:15:11 -0500

    Notes on how to do more segmentation

--- old/Segmentation2.py
+++ old/Segmentation2.py
@@ -2,6 +2,12 @@ import numpy as np
 import pylab
 import mahotas as mh
 
+## (TODO) Loop over each image in a directory
+  ## (TODO) Load both nuclear (DAPI) and cytoplasm (SE) images
+
+  ## (TODO) Find seeds on the nuclear image
+  ## (TODO) Watershed on the cytoplasm image
+
 dna = mh.imread('dna.jpeg') # convert from memory to disk array
 dna = dna.squeeze() # change image array from 3d to 2d
 pylab.gray() # convert to gray scale
@@ -38,6 +44,18 @@ dnaw[dnat] = 0
 pylab.imshow(dnaw)
 pylab.jet() # makes image colourful
 pylab.show()
+
+## (TODO) Loop over each cell found
+  ## (TODO) Save cell to CSV with:
+   # 1. a random UUID ()
+   # 2. list of pixels in cell boundries 
+    # single_cell_img = im == id
+    # cell_perim = perim(im)
+    # locations = nonzero(cell_perim)
+    # ravelled_locs = np.ravel_multi_index(locations, im.shape)
+  # 3. filename of the image that the cell was found in
+
+
 
 
 
